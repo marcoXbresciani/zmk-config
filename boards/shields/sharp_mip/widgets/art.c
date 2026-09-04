c6ac8623e5ee8a9eb4ec05411907caea8e6375e3
Marco Bresciani <48018419+marcoXbresciani@users.noreply.github.com>
2026-09-04 14:54:01 +0200

    Update art.c

--- boards/shields/sharp_mip/widgets/art.c
+++ boards/shields/sharp_mip/widgets/art.c
@@ -12,7 +12,7 @@
 #include "atlas_wob.c"
 #include "bonsai_bow.c"
 #include "dungeon_wob.c"
-#include "fuji.c"
 #include "enterprise_wob.c"
+#include "fuji.c"
 #include "openworld_wob.c"
 #include "space_invaders_wob.c"
